cb10bf76d72cfc0e6cad328c3fedd1af0de4e89d
chrisr3d <chris.studer.68@gmail.com>
2020-10-07 00:19:19 +0200

    wip: [tests] Added stix1 export tests for events with tags and event with attack-pattern galaxies
    - More tests for events with other galaxy types to come as well

--- tests/test_events.py
+++ tests/test_events.py
@@ -23,6 +23,25 @@ _BASE_EVENT = {
     }
 }
 
+_TEST_ATTACK_PATTERN = {
+    "uuid": "c4e851fa-775f-11e7-8163-b774922098cd",
+    "name": "Attack Pattern",
+    "type": "mitre-attack-pattern",
+    "description": "ATT&CK Tactic",
+    "GalaxyCluster": [
+        {
+            "uuid": "dcaa092b-7de9-4a21-977f-7fcb77e89c48",
+            "value": "Access Token Manipulation - T1134",
+            "description": "Windows uses access tokens to determine the ownership of a running process.",
+            "meta": {
+                "external_id": [
+                    "CAPEC-633"
+                ]
+            }
+        }
+    ]
+}
+
 
 def get_base_event():
     return deepcopy(_BASE_EVENT)
@@ -33,3 +52,24 @@ def get_published_event():
     base_event['published'] = True,
     base_event['publish_timestamp'] = str(int(datetime.now().timestamp()))
     return base_event
+
+
+def get_event_with_tags():
+    event = deepcopy(_BASE_EVENT)
+    event['Tag'] = [
+        {"name": "tlp:white"},
+        {"name": 'misp:tool="misp2stix"'},
+        {"name": 'misp-galaxy:mitre-attack-pattern="Code Signing - T1116"'},
+        {"name": 'misp-galaxy:mitre-attack-pattern="Access Token Manipulation - T1134"'}
+    ]
+    event['Galaxy'] = [
+        deepcopy(_TEST_ATTACK_PATTERN)
+    ]
+    return event
+
+def get_event_with_attack_pattern_galaxy():
+    event = deepcopy(_BASE_EVENT)
+    event['Galaxy'] = [
+        deepcopy(_TEST_ATTACK_PATTERN)
+    ]
+    return event
--- tests/test_stix1_export.py
+++ tests/test_stix1_export.py
@@ -15,21 +15,27 @@ class TestStix1Export(unittest.TestCase):
     def setUp(self):
         self.parser = MISPtoSTIX1Parser(_DEFAULT_NAMESPACE, _DEFAULT_ORGNAME)
 
+    @staticmethod
+    def get_marking_value(marking):
+        if marking._XSI_TYPE == 'tlpMarking:TLPMarkingStructureType')
+            return marking.color
+        return marking.statement
+
     def test_base_event(self):
         event = get_base_event()
         uuid = event['Event']['uuid']
         timestamp = int(event['Event']['timestamp'])
         self.parser.parse_misp_event(event, '1.1.1')
         stix_package = self.parser.stix_package
-        self.assertEqual(stix_package.id_, f"{_DEFAULT_ORGNAME}:STIXPackage-{uuid}")
+        self.assertEqual(stix_package.id_, f"{_DEFAULT_NAMESPACE}:STIXPackage-{uuid}")
         self.assertEqual(int(stix_package.timestamp.timestamp()), timestamp)
         self.assertEqual(stix_package.version, '1.1.1')
         self.assertEqual(stix_package.stix_header.title, f'Export from {_DEFAULT_NAMESPACE} MISP')
         incident = stix_package.incidents[0]
-        self.assertEqual(incident.id_, f"{_DEFAULT_ORGNAME}:STIXPackage-{uuid}")
+        self.assertEqual(incident.id_, f"{_DEFAULT_NAMESPACE}:STIXPackage-{uuid}")
         self.assertEqual(incident.title, event['Event']['info'])
-        self.assertEqual(incident.information_source.identity.name, event['Event']['Orgc']['name'])
-        self.assertEqual(incident.reporter.identity.name, event['Event']['Org']['name'])
+        self.assertEqual(incident.information_source.identity.name, _DEFAULT_ORGNAME)
+        self.assertEqual(incident.reporter.identity.name, _DEFAULT_ORGNAME)
 
     def test_published_event(self):
         event = get_published_event()
@@ -40,3 +46,31 @@ class TestStix1Export(unittest.TestCase):
         self.assertEqual(int(incident.timestamp.timestamp()), timestamp)
         self.assertEqual(incident.time.incident_discovery.value.strftime("%Y-%m-%d"), event['Event']['date'])
         self.assertEqual(int(incident.time.incident_reported.value.timestamp()), publish_timestamp)
+
+    def test_event_with_tags(self):
+        event = get_event_with_tags()
+        self.parser.parse_misp_event(event, '1.1.1')
+        marking = self.parser.stix_package.incidents[0].handling[0]
+        self.assertEqual(len(marking.marking_structures), 3)
+        markings = tuple(self.get_marking_value(marking) for marking in marking.marking_structures)
+        self.assertIn('WHITE', markings)
+        self.assertIn('misp:tool="misp2stix"', markings)
+        self.assertIn('misp-galaxy:mitre-attack-pattern="Code Signing - T1116"', markings)
+
+    def test_event_with_attack_pattern_galaxy(self):
+        event = get_event_with_attack_pattern_galaxy()
+        galaxy = event['Galaxy'][0]
+        cluster = galaxy['GalaxyCluster'][]0
+        self.parser.parse_misp_event(event, '1.1.1')
+        stix_package = self.parser.stix_package
+        self.assertEqual(len(stix_package.ttps.ttp), 1)
+        ttp = stix_package.ttps.ttp[0]
+        ttp_id = f"{_DEFAULT_NAMESPACE}:TTP-{cluster['uuid']}"
+        self.assertEqual(ttp.id_, ttp_id)
+        self.assertEqual(ttp.title, f"{galaxy['name']} (MISP Galaxy)")
+        attack_pattern = ttp.behavior.attack_patterns[0]
+        self.assertEqual(attack_pattern.id_, f"{_DEFAULT_NAMESPACE}:AttackPattern-{cluster['uuid']}")
+        self.assertEqual(attack_pattern.title, cluster['value'])
+        related_ttp = stix_package.incidents[0].leveraged_ttps.ttp[0]
+        self.assertEqual(related_ttp.relationship, galaxy['name'])
+        self.assertEqual(related_ttp.item.idref, ttp_id)
