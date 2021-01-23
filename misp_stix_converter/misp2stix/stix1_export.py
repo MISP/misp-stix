8fb40ce04441907a89218656dc03dd0e16a1b165
chrisr3d <chris.studer.68@gmail.com>
2021-01-23 01:08:41 +0100

    wip: [stix1 export] Better way to extract the stix packages

--- misp_stix_converter/misp2stix/stix1_export.py
+++ misp_stix_converter/misp2stix/stix1_export.py
@@ -10,7 +10,7 @@ from .misp_to_stix1 import MISPtoSTIX1Parser
 
 
 class Stix1ExportParser(ExportParser):
-    def __init__(self, return_format, namespace, org, include_namespaces=True):
+    def __init__(self, return_format, namespace, org, include_namespaces):
         self._return_format = return_format
         self._orgname = org
         self._include_namespaces = include_namespaces
@@ -26,6 +26,7 @@ class Stix1ExportParser(ExportParser):
     def generate_stix1_package(self, version):
         if self.json_event.get('response'):
             self._stix_package = STIXPackage()
+            self._stix_package.version = version
             for event in self.json_event['response']:
                 package_generator = MISPtoSTIX1Parser(self._namespace, self._orgname)
                 package_generator.parse_misp_event(event, version)
@@ -39,21 +40,16 @@ class Stix1ExportParser(ExportParser):
     def stix_package(self):
         return self._stix_package
 
-    ################################################################################
-    ##                             UTILITY FUNCTIONS.                             ##
-    ################################################################################
-
-    @staticmethod
-    def _extract_event(event):
-        if event.get('Event'):
-            return event['Event']
-        return event
-
-    def format_to_package(self, args={}):
+    @property
+    def decoded_package(self):
         if self._return_format == 'xml':
-            args = {
-                'include_namespaces': self._include_namespaces,
-                'include_schemalocs': self._include_namespaces,
-                'encoding': 'utf8'
-            }
-        return f'to_{self._return_format}', args
+            return self._stix_package.to_xml(**self.xml_args)
+        return self._stix_package.to_dict()
+
+    @property
+    def xml_args(self):
+        return {
+            'include_namespaces': self._include_namespaces,
+            'include_schemalocs': self._include_namespaces,
+            'encoding': 'utf8'
+        }
