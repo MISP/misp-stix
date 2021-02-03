f9b7e62dd7b175b6df22f8e7e8e5f1b3e740b4ba
chrisr3d <chris.studer.68@gmail.com>
2021-02-04 00:05:54 +0100

    wip: [stix2 export] Fixed STIX 2.0 & 2.1 import in init file

--- misp_stix_converter/misp2stix/__init__.py
+++ misp_stix_converter/misp2stix/__init__.py
@@ -1,4 +1,5 @@
 from .framing import stix_framing, stix20_framing
 from .misp_to_stix1 import MISPtoSTIX1Parser
-from .misp_to_stix2 import MISPtoSTIX20Parser, MISPtoSTIX21Parser
+from .misp_to_stix20 import MISPtoSTIX20Parser
+from .misp_to_stix21 import MISPtoSTIX21Parser
 from .stix1_export import Stix1ExportParser
