09a047eea79f2de50a3cd8adc9b90fc3021cb552
chrisr3d <chris.studer.68@gmail.com>
2021-11-11 19:50:32 +0100

    fix: [stix1 export] Making the STIX1 content getter functions callable

--- misp_stix_converter/__init__.py
+++ misp_stix_converter/__init__.py
@@ -1,4 +1,7 @@
 __version__ = '0.1'
+
+import argparse
 from .misp2stix import *
 from .misp_stix_converter import misp_attribute_collection_to_stix1, misp_collection_to_stix2_0, misp_collection_to_stix2_1, misp_event_collection_to_stix1, misp_to_stix1, misp_to_stix2_0, misp_to_stix2_1
-from .misp_stix_converter import _get_json_events, _get_xml_events
+from .misp_stix_converter import _get_json_campaigns, _get_json_events, _get_json_indicators, _get_json_ttps
+from .misp_stix_converter import _get_xml_campaigns, _get_xml_events, _get_xml_indicators, _get_xml_ttps
