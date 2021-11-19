f1aba6d9baf8f6d81a78c165fdce0f8f55706a4c
chrisr3d <chris.studer.68@gmail.com>
2021-11-19 21:12:33 +0100

    fix: [stix1 framing] Making sure we properly set the Package id in the framing

--- misp_stix_converter/misp2stix/framing.py
+++ misp_stix_converter/misp2stix/framing.py
@@ -72,14 +72,18 @@ def _stix_json_framing(stix_package: STIXPackage) -> tuple:
     return header, ', ', ']}}'
 
 
-def _stix_package(orgname: str, version: str) -> tuple:
-    stix_package = STIXPackage()
+def _stix_package(orgname: str, version: str, uuid: Optional[str] = None) -> STIXPackage:
+    if uuid is None:
+        uuid = uuid4()
+    stix_package = STIXPackage(
+        id_=f'{orgname}:STIXPackage-{uuid}',
+        timestamp=datetime.datetime.now()
+    )
+    stix_package.version = version
     stix_header = STIXHeader()
     stix_header.title = f"Export from {orgname}'s MISP"
     stix_header.package_intents = 'Threat Report'
     stix_package.stix_header = stix_header
-    stix_package.version = version
-    stix_package.timestamp = datetime.datetime.now()
     return stix_package
 
 
