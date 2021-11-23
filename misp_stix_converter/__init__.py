6e54c718d27ee1bcbd6f356ad14a7e69fc31476b
chrisr3d <chris.studer.68@gmail.com>
2021-11-23 23:35:24 +0100

    fix: [stix export] Making the formatting functions available

--- misp_stix_converter/__init__.py
+++ misp_stix_converter/__init__.py
@@ -4,5 +4,5 @@ import argparse
 from .misp_stix_mapping import Mapping
 from .misp2stix import *
 from .misp_stix_converter import misp_attribute_collection_to_stix1, misp_collection_to_stix2_0, misp_collection_to_stix2_1, misp_event_collection_to_stix1, misp_to_stix1, misp_to_stix2_0, misp_to_stix2_1
-from .misp_stix_converter import _get_json_campaigns, _get_json_events, _get_json_indicators, _get_json_observables, _get_json_ttps
-from .misp_stix_converter import _get_xml_campaigns, _get_xml_events, _get_xml_indicators, _get_xml_observables, _get_xml_ttps
+from .misp_stix_converter import _get_json_campaigns, _get_json_courses_of_action, _get_json_events, _get_json_indicators, _get_json_observables, _get_json_threat_actors, _get_json_ttps
+from .misp_stix_converter import _get_xml_campaigns, _get_xml_courses_of_action, _get_xml_events, _get_xml_indicators, _get_xml_observables, _get_xml_threat_actors, _get_xml_ttps
