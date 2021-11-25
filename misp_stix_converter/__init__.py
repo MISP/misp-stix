189732150b47fdf2770cee358b8104f3674e4302
chrisr3d <chris.studer.68@gmail.com>
2021-11-25 14:48:03 +0100

    fix: [stix1 export] Making all the STIX objects header and footer helper functions available

--- misp_stix_converter/__init__.py
+++ misp_stix_converter/__init__.py
@@ -4,5 +4,6 @@ import argparse
 from .misp_stix_mapping import Mapping
 from .misp2stix import *
 from .misp_stix_converter import misp_attribute_collection_to_stix1, misp_collection_to_stix2_0, misp_collection_to_stix2_1, misp_event_collection_to_stix1, misp_to_stix1, misp_to_stix2_0, misp_to_stix2_1
-from .misp_stix_converter import _get_json_campaigns, _get_json_courses_of_action, _get_json_events, _get_json_indicators, _get_json_observables, _get_json_threat_actors, _get_json_ttps
-from .misp_stix_converter import _get_xml_campaigns, _get_xml_courses_of_action, _get_xml_events, _get_xml_indicators, _get_xml_observables, _get_xml_threat_actors, _get_xml_ttps
+from .misp_stix_converter import _get_campaigns, _get_courses_of_action, _get_events, _get_indicators, _get_observables, _get_threat_actors, _get_ttps
+from .misp_stix_converter import _get_campaigns_footer, _get_courses_of_action_footer, _get_indicators_footer, _get_observables_footer, _get_threat_actors_footer, _get_ttps_footer
+from .misp_stix_converter import _get_campaigns_header, _get_courses_of_action_header, _get_indicators_header, _get_observables_header, _get_threat_actors_header, _get_ttps_footer
