d7f6ae47fe5261553df22c40757b3ee9fb00c47d
chrisr3d <chris.studer.68@gmail.com>
2022-01-25 19:41:34 +0100

    wip: [stix2 import] We start the STIX2 import
    - From pseudo-code draft & ideas in mind

---
+++ misp_stix_converter/stix2misp/__init__.py
@@ -0,0 +1,4 @@
+from .external_stix1_to_misp import ExternalSTIX1toMISPParser
+from .external_stix2_to_misp import ExternalSTIX2toMISPParser
+from .internal_stix1_to_misp import InternalSTIX1toMISPParser
+from .internal_stix2_to_misp import InternalSTIX2toMISPParser
