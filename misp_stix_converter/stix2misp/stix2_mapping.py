1a099c2299c635449f5bea4c19d4c5bce615f141
chrisr3d <chris.studer.68@gmail.com>
2022-01-28 23:51:05 +0100

    wip: [stix2 import] Main STIX2 objects parsing functions mapping

--- misp_stix_converter/stix2misp/stix2_mapping.py
+++ misp_stix_converter/stix2misp/stix2_mapping.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from .. import Mapping
+from typing import Optional
 
 
 class STIX2Mapping:
@@ -15,6 +16,34 @@ class STIX2Mapping:
             ' REPEATS '
         )
 
+    def _declare_mapping(self, updates=Optional[dict]=None):
+        self.__stix_to_misp_mapping = Mapping(
+            **{
+                'attack-pattern': '_load_attack_pattern',
+                'course-of-action': '_load_course_of_action',
+                'grouping': '_load_grouping',
+                'identity': '_load_identity',
+                'indicator': '_parse_indicator',
+                'intrusion-set': '_load_intrusion_set',
+                'malware': '_load_malware',
+                'marking-definition': '_load_marking_definition',
+                'note': '_parse_note',
+                'observed-data': '_load_observed_data',
+                'relationship': '_load_relationship',
+                'report': '_load_report',
+                'threat-actor': '_load_threat_actor',
+                'tool': '_load_tool',
+                'vulnerability': '_load_vulnerability',
+                'x-misp-attribute': '_parse_custom_attribute',
+                'x-misp-object': '_parse_custom_object'
+            }
+        )
+
+
     @property
     def pattern_forbidden_relations(self) -> tuple:
-        self.__pattern_forbidden_relations
+        return self.__pattern_forbidden_relations
+
+    @property
+    def stix_to_misp_mapping(self) -> dict:
+        return self.__stix_to_misp_mapping
