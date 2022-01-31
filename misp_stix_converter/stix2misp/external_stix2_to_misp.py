eb6581f997fe883b18f34a71183f22e0652db5c4
chrisr3d <chris.studer.68@gmail.com>
2022-01-31 17:31:55 +0100

    wip: [stix2 import] We continue building the stix2 import skeleton

--- misp_stix_converter/stix2misp/external_stix2_to_misp.py
+++ misp_stix_converter/stix2misp/external_stix2_to_misp.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from .external_stix2_mapping import ExternalSTIX2Mapping
 from .stix2_to_misp import STIX2toMISPParser
 
 
 class ExternalSTIX2toMISPParser(STIX2toMISPParser):
     def __init__(self):
         super().__init__()
+        self._mapping = ExternalSTIX2Mapping()
