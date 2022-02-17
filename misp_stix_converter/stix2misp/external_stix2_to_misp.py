f08e870abad1036e9e3964238dfbe590b2330c52
chrisr3d <chris.studer.68@gmail.com>
2022-02-17 13:53:46 +0100

    wip: [stix2 import] Some pieces of documentation for the main parsing function used for external STIX 2

--- misp_stix_converter/stix2misp/external_stix2_to_misp.py
+++ misp_stix_converter/stix2misp/external_stix2_to_misp.py
@@ -20,11 +20,32 @@ class ExternalSTIX2toMISPParser(STIX2toMISPParser):
     ################################################################################
 
     def _handle_observable_mapping(self, observed_data: ObservedData_v21) -> str:
+        """
+        Takes a STIX 2.1 Observed Data object and redirects to the appropriate
+        parsing function depending on the observable objects fields used: objects or
+        object_refs.
+
+        :param observed_data: A STIX 2.1 Observed Data object
+        :return: The name of the parsing function which should be used to convert
+            observable objects and their related observable objects into MISP
+            attributes or objects
+        """
         if hasattr(observed_data, 'object_refs'):
             return self._handle_observable_refs_mapping(observed_data.object_refs)
         return self._handle_observable_objects_mapping(observed_data.objects)
 
     def _handle_observable_objects_mapping(self, observable_objects: dict) -> str:
+        """
+        Takes Observable objects to extract each observable object type in order to
+        build a tuple which will be used to map with the appropriate parsing
+        function.
+
+        :param observable_objects: Observable objects as a dict like preconised by
+            the STIX 2.0 standard. Could be either STIX 2.0 or STIX 2.1 (deprecated)
+            observable objects.
+        :return: The name of the parsing function mapped from the observable objects
+            types extracted from the observable objects
+        """
         observable_types = self._extract_types_from_observable_objects(observable_objects)
         try:
             feature = self._mapping.observable_mapping[observable_types]
@@ -33,6 +54,16 @@ class ExternalSTIX2toMISPParser(STIX2toMISPParser):
         return f'{feature}_objects'
 
     def _handle_observable_refs_mapping(self, observable_refs: list) -> str:
+        """
+        Takes Observable object references to extract each observable object type
+        from the references observable objects in order to build a tuple which will
+        be used to map with the appropriate parsing function.
+
+        :param observable_refs: List of observable object ids referenced by an
+            observed data object
+        :return: The name of the parsing function mapped from the observable objects
+            types extracted from the referenced observable objects.
+        """
         observable_types = self._extract_types_from_observable_refs(observable_refs)
         try:
             feature = self._mapping.observable_mapping[observable_types]
@@ -62,6 +93,13 @@ class ExternalSTIX2toMISPParser(STIX2toMISPParser):
 
     def _parse_indicator(self, indicator_ref: str):
         """
+        Indicators parsing function. Gets the parsing function depending on the
+        types found within the pattern, and simply calls the parsing function that
+        will simply parse the pattern, create the appropriate MISP data structure
+        and add it to the associated MISP event.
+
+        :param indicator_ref: An indicator id used to find the corresponding
+            indicator object
         """
         indicator = self._get_stix_object(indicator_ref)
         try:
@@ -83,6 +121,13 @@ class ExternalSTIX2toMISPParser(STIX2toMISPParser):
 
     def _parse_observed_data_v20(self, observed_data: ObservedData_v20):
         """
+        STIX 2.0 Observed Data parsing function. Gets the parsing function depending
+        on the observable object types found within the observable objects, and
+        simply calls the parsing function that will simply parse the observable
+        objects, create the appropriate MISP data structure and add it to the
+        associated MISP event.
+
+        :param observed_data: The STIX 2.0 Observed Data object to parse
         """
         feature = self._handle_observable_objects_mapping(observed_data.objects)
         try:
@@ -96,8 +141,17 @@ class ExternalSTIX2toMISPParser(STIX2toMISPParser):
 
     def _parse_observed_data_v21(self, observed_data: ObservedData_v21):
         """
-        """
+        STIX 2.1 Observed Data parsing function. Gets the parsing function depending
+        on the observable object types found either within the observable objects
+        referenced in the object_refs fields, or the observable objects embedded in
+        the observed data. It simply calls the parsing function then in order to
+        parse the observed data and its related observable objects, and create the
+        appropriate MISP data structure that will be added to the associate MISP
+        event.
 
+        :param observed_data: The STIX 2.1 Observed Data object to parse
+        """
+        feature = self._handle_observable_mapping(observed_data)
         try:
             parser = getattr(self, feature)
         except AttributeError:
