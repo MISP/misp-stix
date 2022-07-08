207e36cb2252da10b5381e0fed3b20c1b6606fdc
Christian Studer <christian.studer@circl.lu>
2022-07-08 23:59:40 +0200

    wip: [stix2 import] We'll use strings to map external observable types instead of tuples

--- misp_stix_converter/stix2misp/external_stix2_mapping.py
+++ misp_stix_converter/stix2misp/external_stix2_mapping.py
@@ -26,21 +26,21 @@ class ExternalSTIX2Mapping(STIX2Mapping):
 
         # MAIN STIX OBJECTS MAPPING
         observable_mapping = {
-            ('domain-name', 'network-traffic'): '_parse_domain_network_traffic_observable',
-            ('email-addr',): '_parse_email_address_observable',
-            ('mac-addr',): '_parse_mac_address_observable',
-            ('mutex',): 'parse_mutex_observable',
-            ('x509-certificate',): 'parse_x509_observable',
-            ('user-account',): 'parse_user_account_observable',
-            ('windows-registry-key',): 'parse_regkey_observable'
+            'domain-name_network-traffic': '_parse_domain_network_traffic_observable',
+            'email-addr': '_parse_email_address_observable',
+            'mac-addr': '_parse_mac_address_observable',
+            'mutex': 'parse_mutex_observable',
+            'x509-certificate': 'parse_x509_observable',
+            'user-account': 'parse_user_account_observable',
+            'windows-registry-key': 'parse_regkey_observable'
         }
         observable_mapping.update(
             dict.fromkeys(
                 (
-                    ('artifact', 'file'),
-                    ('artifact', 'directory', 'file'),
-                    ('directory', 'file'),
-                    ('file',)
+                    'artifact_file',
+                    'artifact_directory_file',
+                    'directory_file',
+                    'file'
                 ),
                 '_parse_file_observable'
             )
@@ -48,10 +48,13 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         observable_mapping.update(
             dict.fromkeys(
                 (
-                    ('artifact', 'email-addr', 'email-message', 'file'),
-                    ('email-addr', 'email-message'),
-                    ('email-addr', 'email-message', 'file'),
-                    ('email-message',)
+                    'artifact_email-addr_email-message',
+                    'artifact_email-addr_email-message_file',
+                    'artifact_email-message',
+                    'email-addr_email-message',
+                    'email-addr_email-message_file',
+                    'email-message',
+                    'email-message_file'
                 ),
                 '_parse_email_observable'
             )
@@ -59,10 +62,10 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         observable_mapping.update(
             dict.fromkeys(
                 (
-                    ('autonomous-system',),
-                    ('autonomous-system', 'ipv4-addr'),
-                    ('autonomous-system', 'ipv6-addr'),
-                    ('autonomous-system', 'ipv4-addr', 'ipv6-addr')
+                    'autonomous-system',
+                    'autonomous-system_ipv4-addr',
+                    'autonomous-system_ipv6-addr',
+                    'autonomous-system-ipv4-addr_ipv6-addr'
                 ),
                 '_parse_asn_observable'
             )
@@ -70,10 +73,10 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         observable_mapping.update(
             dict.fromkeys(
                 (
-                    ('domain-name',),
-                    ('domain-name', 'ipv4-addr'),
-                    ('domain-name', 'ipv6-addr'),
-                    ('domain-name', 'ipv4-addr', 'ipv6-addr')
+                    'domain-name',
+                    'domain-name_ipv4-addr',
+                    'domain-name_ipv6-addr',
+                    'domain-name_ipv4-addr_ipv6-addr'
                 ),
                 '_parse_domain_ip_observable'
             )
@@ -81,9 +84,9 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         observable_mapping.update(
             dict.fromkeys(
                 (
-                    ('domain-name', 'ipv4-addr', 'network-traffic'),
-                    ('domain-name', 'ipv6-addr', 'network-traffic'),
-                    ('domain-name', 'ipv4-addr', 'ipv6-addr', 'network-traffic')
+                    'domain-name_ipv4-addr_network-traffic',
+                    'domain-name_ipv6-addr_network-traffic',
+                    'domain-name_ipv4-addr_ipv6-addr_network-traffic'
                 ),
                 '_parse_domain_ip_network_traffic_observable'
             )
@@ -91,8 +94,8 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         observable_mapping.update(
             dict.fromkeys(
                 (
-                    ('domain-name', 'network-traffic', 'url'),
-                    ('url',)
+                    'domain-name_network-traffic_url',
+                    'url'
                 ),
                 '_parse_url_observable'
             )
@@ -100,8 +103,8 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         observable_mapping.update(
             dict.fromkeys(
                 (
-                    ('file', 'process'),
-                    ('process',)
+                    'file_process',
+                    'process'
                 ),
                 '_parse_process_observable'
             )
@@ -109,8 +112,8 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         observable_mapping.update(
             dict.fromkeys(
                 (
-                    ('ipv4-addr',),
-                    ('ipv6-addr',),
+                    'ipv4-addr',
+                    'ipv6-addr'
                 ),
                 '_parse_ip_address_observable'
             )
@@ -118,32 +121,32 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         observable_mapping.update(
             dict.fromkeys(
                 (
-                    ('ipv4-addr', 'network-traffic'),
-                    ('ipv6-addr', 'network-traffic'),
-                    ('ipv4-addr', 'ipv6-addr', 'network-traffic')
+                    'ipv4-addr_network-traffic',
+                    'ipv6-addr_network-traffic',
+                    'ipv4-addr_ipv6-addr_network-traffic'
                 ),
                 '_parse_parse_ip_network_traffic_observable'
             )
         )
         self.__observable_mapping = Mapping(**observable_mapping)
         pattern_mapping = {
-            ('email-addr',): 'parse_email_address_pattern',
-            ('email-message',): 'parse_email_message_pattern',
-            ('mac-addr',): 'parse_mac_address_pattern',
-            ('mutex',): 'parse_mutex_pattern',
-            ('network-traffic',): 'parse_network_traffic_pattern',
-            ('process',): 'parse_process_pattern',
-            ('user-account',): 'parse_user_account_pattern',
-            ('windows-registry-key',): 'parse_regkey_pattern',
-            ('x509-certificate',): 'parse_x509_pattern'
+            'email-addr': 'parse_email_address_pattern',
+            'email-message': 'parse_email_message_pattern',
+            'mac-addr': 'parse_mac_address_pattern',
+            'mutex': 'parse_mutex_pattern',
+            'network-traffic': 'parse_network_traffic_pattern',
+            'process': 'parse_process_pattern',
+            'user-account': 'parse_user_account_pattern',
+            'windows-registry-key': 'parse_regkey_pattern',
+            'x509-certificate': 'parse_x509_pattern'
         }
         pattern_mapping.update(
             dict.fromkeys(
                 (
-                    ('artifact', 'file'),
-                    ('directory',),
-                    ('directory', 'file'),
-                    ('file',)
+                    'artifact_file',
+                    'directory',
+                    'directory_file',
+                    'file'
                 ),
                 '_parse_file_pattern'
             )
@@ -151,10 +154,10 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         pattern_mapping.update(
             dict.fromkeys(
                 (
-                    ('autonomous-system', ),
-                    ('autonomous-system', 'ipv4-addr'),
-                    ('autonomous-system', 'ipv6-addr'),
-                    ('autonomous-system', 'ipv4-addr', 'ipv6-addr')
+                    'autonomous-system',
+                    'autonomous-system_ipv4-addr',
+                    'autonomous-system_ipv6-addr',
+                    'autonomous-system_ipv4-addr_ipv6-addr'
                 ),
                 '_parse_asn_pattern'
             )
@@ -162,11 +165,11 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         pattern_mapping.update(
             dict.fromkeys(
                 (
-                    ('domain-name',),
-                    ('domain-name', 'ipv4-addr'),
-                    ('domain-name', 'ipv6-addr'),
-                    ('domain-name', 'ipv4-addr', 'ipv6-addr'),
-                    ('domain-name', 'network-traffic')
+                    'domain-name',
+                    'domain-name_ipv4-addr',
+                    'domain-name_ipv6-addr',
+                    'domain-name_ipv4-addr_ipv6-addr',
+                    'domain-name_network-traffic'
                 ),
                 '_parse_domain_ip_port_pattern'
             )
@@ -174,11 +177,11 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         pattern_mapping.update(
             dict.fromkeys(
                 (
-                    ('domain-name', 'ipv4-addr', 'url'),
-                    ('domain-name', 'ipv6-addr', 'url'),
-                    ('domain-name', 'ipv4-addr', 'ipv6-addr', 'url'),
-                    ('domain-name', 'network-traffic', 'url'),
-                    ('url',)
+                    'domain-name_ipv4-addr_url',
+                    'domain-name_ipv6-addr_url',
+                    'domain-name_ipv4-addr_ipv6-addr_url',
+                    'domain-name_network-traffic_url',
+                    'url'
                 ),
                 '_parse_url_pattern'
             )
@@ -186,9 +189,9 @@ class ExternalSTIX2Mapping(STIX2Mapping):
         pattern_mapping.update(
             dict.fromkeys(
                 (
-                    ('ipv4-addr',),
-                    ('ipv6-addr',),
-                    ('ipv4-addr', 'ipv6-addr')
+                    'ipv4-addr',
+                    'ipv6-addr',
+                    'ipv4-addr_ipv6-addr'
                 ),
                 '_parse_ip_address_pattern'
             )
