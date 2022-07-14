51d029a1b691e92568e817bb9b4fdd556b5c6777
Christian Studer <christian.studer@circl.lu>
2022-07-14 16:13:04 +0200

    fix: [stix2 export] Fixed the path of the submodule used to fetch already existing STIX cti objects

--- misp_stix_converter/misp2stix/misp_to_stix2.py
+++ misp_stix_converter/misp2stix/misp_to_stix2.py
@@ -182,7 +182,7 @@ class MISPtoSTIX2Parser(MISPtoSTIXParser):
 
     def _generate_galaxies_catalog(self):
         current_path = Path(os.path.dirname(os.path.realpath(__file__)))
-        cti_path = current_path.parent.parent / 'data' / 'cti'
+        cti_path = current_path.parent / 'data' / 'cti'
         self._galaxies_catalog = defaultdict(lambda: defaultdict(list))
         self._identities = {}
         for filename in cti_path.glob('*/*.json'):
