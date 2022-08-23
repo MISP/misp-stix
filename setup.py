9356bde289277120134bc75d57dfad72efa20885
Christian Studer <christian.studer@circl.lu>
2022-08-23 14:24:46 +0200

    chg: [package] Updated some setup information

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ with open(path.join(this_directory, 'README.md'), 'r') as f:
     long_description = f.read()
 
 setup(
-    name='misp_stix_converter',
+    name='misp-stix',
     version=misp_stix_converter.__version__,
     author='Christian Studer',
     author_email='christian.studer@circl.lu',
     maintainer='Christian Studer',
-    url='https://github.com/chrisr3d/misp-stix-converter',
+    url='https://github.com/MISP/misp-stix',
     project_urls={
-        'Documentation': 'https://github.com/chrisr3d/misp-stix-converter/documentation',
-        'Source': 'https://github.com/chrisr3d/misp-stix-converter',
-        'Tracker': 'https://github.com/chrisr3d/misp-stix-converter/issues'
+        'Documentation': 'https://github.com/MISP/misp-stix/documentation',
+        'Source': 'https://github.com/MISP/misp-stix',
+        'Tracker': 'https://github.com/MISP/misp-stix/issues'
     },
     description='Python scripts to convert MISP into STIX or STIX into MISP',
     long_description=long_description,
@@ -28,7 +28,7 @@ setup(
     entry_points={"console_scripts": ["misp_stix_converter = misp_stix_converter:main"]},
     classifiers=[
         'License :: OSI Approved :: BSD License',
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Operating System :: POSIX :: Linux',
         'Intended Audience :: Science/Research',
@@ -40,5 +40,6 @@ setup(
         'Topic :: Security',
         'Topic :: Internet',
     ],
-    install_requires=['pymisp', 'stix', 'stix2']
+    install_requires=['pymisp', 'stix', 'misp-lib-stix2']
+    tests_require=['pytest', 'flake8']
 )
