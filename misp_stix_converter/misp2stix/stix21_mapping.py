76f4e6f58fa332e3b9170a20151aca762df16dca
Christian Studer <christian.studer@circl.lu>
2023-02-10 23:32:09 +0100

    wip: [stix2 export] Simplified the Galaxies mapping

--- misp_stix_converter/misp2stix/stix21_mapping.py
+++ misp_stix_converter/misp2stix/stix21_mapping.py
@@ -23,14 +23,8 @@ class MISPtoSTIX21Mapping(MISPtoSTIX2Mapping):
             )
         )
         v21_specific_galaxy_mapping = {
-            'cluster_to_stix_object': {
-                'country': 'location',
-                'region': 'location'
-            },
-            'galaxy_types_mapping': {
-                'country': '_parse_location_{}_galaxy',
-                'region': '_parse_location_{}_galaxy'
-            }
+            'country': 'location',
+            'region': 'location'
         }
         self._declare_attributes_mapping(
             attribute_updates=v21_specific_attribute_mapping,
