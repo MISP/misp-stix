4ae0cad8330cd000a3a09d5c6cd2e3f0a2cd0f74
Christian Studer <christian.studer@circl.lu>
2023-04-12 15:36:24 +0200

    chg: [misp_stix_converter] Added quick comments & made the `_from_misp` utility available to import from the library

--- misp_stix_converter/__init__.py
+++ misp_stix_converter/__init__.py
@@ -3,19 +3,24 @@ __version__ = '2.4.169'
 import argparse
 from .misp_stix_mapping import Mapping
 from .misp2stix import *
+# Helpers
 from .misp_stix_converter import (
-    misp_attribute_collection_to_stix1, misp_collection_to_stix2_0,
+    _from_misp, misp_attribute_collection_to_stix1, misp_collection_to_stix2_0,
     misp_collection_to_stix2_1, misp_event_collection_to_stix1, misp_to_stix1,
     misp_to_stix2_0, misp_to_stix2_1, stix_1_to_misp, stix_2_to_misp)
+# STIX 1 special halpers
 from .misp_stix_converter import (
     _get_campaigns, _get_courses_of_action, _get_events, _get_indicators,
     _get_observables, _get_threat_actors, _get_ttps)
+# STIX 1 footers
 from .misp_stix_converter import (
     _get_campaigns_footer, _get_courses_of_action_footer, _get_indicators_footer,
     _get_observables_footer, _get_threat_actors_footer, _get_ttps_footer)
+# STIX 1 headers
 from .misp_stix_converter import (
     _get_campaigns_header, _get_courses_of_action_header, _get_indicators_header,
     _get_observables_header, _get_threat_actors_header, _get_ttps_header)
+# Command line methods
 from .misp_stix_converter import _misp_to_stix, _stix_to_misp
 from .stix2misp import *
 from pathlib import Path
