0117e5eac16242a447cfc8071548fae7f4d8cb8b
Christian Studer <christian.studer@circl.lu>
2023-08-23 15:30:20 +0200

    fix: [stix2 import] Some pycodestyle clean-up

--- misp_stix_converter/stix2misp/converters/__init__.py
+++ misp_stix_converter/stix2misp/converters/__init__.py
@@ -1,7 +1,7 @@
-from .stix2_attack_pattern_converter import ( # noqa
+from .stix2_attack_pattern_converter import (  # noqa
     ExternalSTIX2AttackPatternConverter, InternalSTIX2AttackPatternConverter)
-from .stix2_malware_analysis_converter import ( #noqa
+from .stix2_malware_analysis_converter import (  # noqa
     ExternalSTIX2MalwareAnalysisConverter,
     InternalSTIX2MalwareAnalysisConverter)
-from .stix2_malware_converter import ( # noqa
+from .stix2_malware_converter import (  # noqa
     ExternalSTIX2MalwareConverter, InternalSTIX2MalwareConverter)
